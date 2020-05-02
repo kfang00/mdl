2d027941778d7d7c2685015713f94ef8ea6ce051
Kayla Fang <kfang00@stuy.edu>
2020-05-02 19:39:24 -0400

    made own image

--- makefile
+++ makefile
@@ -1,5 +1,8 @@
 test: face.mdl  main.py matrix.py mdl.py display.py draw.py gmath.py
 	python main.py face.mdl
+	#python main.py test.mdl
+	magick convert pic.ppm image.png
+	imdisplay image.png
 
 clean:
 	rm *pyc *out parsetab.py
--- script.py
+++ script.py
@@ -48,4 +48,98 @@ def run(filename):
 
     print(symbols)
     for command in commands:
-        print(command)
+        #print(command)
+        line = command['op']
+        args = command['args']
+        if line == 'sphere':
+            add_sphere(tmp,
+                       float(args[0]), float(args[1]), float(args[2]),
+                       float(args[3]), step_3d)
+            matrix_mult( stack[-1], tmp )
+            if command['constants'] != None:
+                reflect = command['constants']
+            draw_polygons(tmp, screen, zbuffer, view, ambient, light, symbols, reflect)
+            tmp = []
+            reflect = '.white'
+
+        elif line == 'torus':
+            add_torus(tmp,
+                      float(args[0]), float(args[1]), float(args[2]),
+                      float(args[3]), float(args[4]), step_3d)
+            matrix_mult( stack[-1], tmp )
+            if command['constants'] != None:
+                reflect = command['constants']
+            draw_polygons(tmp, screen, zbuffer, view, ambient, light, symbols, reflect)
+            tmp = []
+            reflect = '.white'
+
+        elif line == 'box':
+            add_box(tmp,
+                    float(args[0]), float(args[1]), float(args[2]),
+                    float(args[3]), float(args[4]), float(args[5]))
+            matrix_mult( stack[-1], tmp )
+            if command['constants'] != None:
+                reflect = command['constants']
+            draw_polygons(tmp, screen, zbuffer, view, ambient, light, symbols, reflect)
+            tmp = []
+            reflect = '.white'
+
+        elif line == 'circle':
+            add_circle(edges,
+                       float(args[0]), float(args[1]), float(args[2]),
+                       float(args[3]), step)
+            matrix_mult( stack[-1], edges )
+            draw_lines(edges, screen, zbuffer, color)
+            edges = []
+
+        elif line == 'hermite' or line == 'bezier':
+            add_curve(edges,
+                      float(args[0]), float(args[1]),
+                      float(args[2]), float(args[3]),
+                      float(args[4]), float(args[5]),
+                      float(args[6]), float(args[7]),
+                      step, line)
+            matrix_mult( stack[-1], edges )
+            draw_lines(edges, screen, zbuffer, color)
+            edges = []
+
+        elif line == 'line':
+            add_edge( edges,
+                      float(args[0]), float(args[1]), float(args[2]),
+                      float(args[3]), float(args[4]), float(args[5]) )
+            matrix_mult( stack[-1], edges )
+            draw_lines(eges, screen, zbuffer, color)
+            edges = []
+
+        elif line == 'scale':
+            t = make_scale(float(args[0]), float(args[1]), float(args[2]))
+            matrix_mult( stack[-1], t )
+            stack[-1] = [ x[:] for x in t]
+
+        elif line == 'move':
+            t = make_translate(float(args[0]), float(args[1]), float(args[2]))
+            matrix_mult( stack[-1], t )
+            stack[-1] = [ x[:] for x in t]
+
+        elif line == 'rotate':
+            theta = float(args[1]) * (math.pi / 180)
+            if args[0] == 'x':
+                t = make_rotX(theta)
+            elif args[0] == 'y':
+                t = make_rotY(theta)
+            else:
+                t = make_rotZ(theta)
+            matrix_mult( stack[-1], t )
+            stack[-1] = [ x[:] for x in t]
+
+        elif line == 'push':
+            stack.append( [x[:] for x in stack[-1]] )
+
+        elif line == 'pop':
+            stack.pop()
+
+        elif line == 'display' or line == 'save':
+            if line == 'display':
+                display(screen)
+            else:
+                save_extension(screen, args[0])
